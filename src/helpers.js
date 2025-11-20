2061ff806d76267b5fbdd6b35430e3414c861287
Mohammed Dwina <diwina1993@hotmail.com>
2025-11-20 12:26:54 +0000

    Remove olde files

--- src/helpers.js
+++
@@ -1,6 +0,0 @@
-export const emailValidate = (email) => {
-  const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  if (reg.test(email)) return true;
-  return false;
-};
